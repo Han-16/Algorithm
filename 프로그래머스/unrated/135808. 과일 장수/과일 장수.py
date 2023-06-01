0e4c7ab0a703dae4e297846a68e1fed96632a19f
ByeongKyu_Han <102290837+Han-16@users.noreply.github.com>
2023-06-01 23:21:54 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -3,5 +3,5 @@ def solution(k, m, score):
     sum_ = 0
     for i in range(0, len(score), m):
         if len(score[i:i+m]) == m:
-            sum_ += min(score[i:i+m]) * m
+            sum_ += score[i:i+m][-1] * m
     return sum_
