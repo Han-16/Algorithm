86da81ce9a942a3035e73c84bcfa19ba5f3f58b6
ByeongKyu_Han <102290837+Han-16@users.noreply.github.com>
2023-06-01 22:57:53 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,10 +1,2 @@
 def solution(name, yearning, photo):
-    na_ye = {v : yearning[i] for i, v in enumerate(name)}
-    answer = []
-    for elem in photo:
-        score = 0
-        for name in elem:
-            if name in na_ye:
-                score += na_ye[name]
-        answer.append(score)
-    return answer
+    return [sum(yearning[name.index(j)] for j in i if j in name) for i in photo]
