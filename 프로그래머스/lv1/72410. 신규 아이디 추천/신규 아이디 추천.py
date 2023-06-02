7294b155ed47bb2ce9f1045172e2a1a9e04c720d
ByeongKyu_Han <102290837+Han-16@users.noreply.github.com>
2023-06-03 00:16:01 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.22 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,24 @@
+def solution(new_id):
+    id_list = list(new_id.lower())                # 1단계
+    new_id = ''
+    
+    for i in id_list:                             # 2단계
+        if i.isalpha() or i.isdigit() or i in ['-', '_', '.']:
+            new_id += i
+    
+    while new_id != new_id.replace('..', '.'):    # 3단계
+        new_id = new_id.replace('..', '.')
+    
+    new_id = new_id.strip('.')                    # 4단계
+    
+    if not new_id:                                # 5단계
+        new_id = 'a'
+    
+    if len(new_id) >= 16:                         # 6단계
+        new_id = new_id[:15].strip('.')
+    
+    if len(new_id) <= 2:                          # 7단계
+        while len(new_id) < 3:
+            new_id = new_id + new_id[-1]
+            
+    return new_id
