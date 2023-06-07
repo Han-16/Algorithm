a7866aba7df0d99b5233170f160b1796e0bd88ec
ByeongKyu_Han <haanbk16@gmail.com>
2023-06-07 17:43:55 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.44 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,10 @@
+def solution(s): 
+    answer = [0, 0]
+    while s != '1':
+        answer[1] += s.count('0')
+        answer[0] += 1
+        s = s.replace('0', '')
+        c = len(s)
+        s = bin(c)[2:]
+    return answer
+    
