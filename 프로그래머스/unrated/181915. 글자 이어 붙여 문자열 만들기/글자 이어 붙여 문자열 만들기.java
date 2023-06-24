69978d0028f04c2408e18a8c00e977da791dda57
ByeongKyu_Han <haanbk16@gmail.com>
2023-06-24 19:51:45 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.06 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        StringBuffer sb = new StringBuffer();
+        for(int index : index_list) {
+           sb.append(my_string.charAt(index));
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
