bc686d3ef8546116f9fa77e40d4179305a13e7bc
ByeongKyu_Han <haanbk16@gmail.com>
2023-07-17 15:15:16 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 18.69 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 32.4 MB, 시간: 63.41 ms
+메모리: 115 MB, 시간: 18.69 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int prev = -1;
+        
+        for (int i : arr) {
+            if (i == prev) {
+                continue;
+            } else {
+                answer.add(i);
+                prev = i;
+            }
+        }
+        return answer;
+    }
+}
