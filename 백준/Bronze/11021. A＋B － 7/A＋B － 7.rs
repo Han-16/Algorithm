f5972e3700e9b860a8fe2e229de2d96116ee1a61
ByeongKyu_Han <haanbk16@gmail.com>
2023-09-02 12:26:13 +0900

    [Bronze V] Title: A+B - 7, Time: 8 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("잘못된 입력");
+    let test_case: i32 = input.trim().parse().expect("정수 입력");
+    input.clear();
+
+    for i in 1..=test_case {
+        io::stdin().read_line(&mut input).expect("잘못된 입력");
+        let numbers: Vec<i32> = input.split_whitespace().map(|s: &str| s.parse().expect("정수 입력")).collect();
+        println!("Case #{}: {}", i, numbers[0] + numbers[1]);
+        input.clear();
+    }
+}
